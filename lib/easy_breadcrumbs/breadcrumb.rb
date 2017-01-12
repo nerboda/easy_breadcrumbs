097043ecf6f9d209cf33f36474c6c506118d01a3
Eliathah Boda <nerboda@gmail.com>
2017-01-11 19:13:37 -0700

    Forgot to place EasyBreadcrumbs module within Sinatra namespace in easy_breadcrumbs.rb

--- lib/easy_breadcrumbs/breadcrumb.rb
+++ lib/easy_breadcrumbs/breadcrumb.rb
@@ -1,12 +1,14 @@
-module EasyBreadcrumbs
-  class Breadcrumb
-    # request_path argument is value returned by Rack::Request#path
-    def initialize(request_path)
-      @request_path = request_path
-    end
+module Sinatra
+  module EasyBreadcrumbs
+    class Breadcrumb
+      # request_path argument is value returned by Rack::Request#path
+      def initialize(request_path)
+        @request_path = request_path
+      end
 
-    def html
-      "<h1>#{@request_path}</h1>"
+      def html
+        "<h1>#{@request_path}</h1>"
+      end
     end
   end
 end
