c517530ecfe23e3c01714948b95e40c4aa6cf20e
Eliathah Boda <nerboda@gmail.com>
2017-01-18 18:35:16 -0700

    Rubocop cleanup.

--- lib/easy_breadcrumbs/breadcrumb.rb
+++ lib/easy_breadcrumbs/breadcrumb.rb
@@ -2,6 +2,7 @@ require 'erubis'
 require 'active_support/inflector/methods'
 
 module EasyBreadcrumbs
+  # Breadcrumb class. Converts a URL path into Bootstrap breadcrumbs
   class Breadcrumb
     include ActiveSupport::Inflector
 
@@ -11,14 +12,14 @@ module EasyBreadcrumbs
       @routes = route_matchers
       @directories = request_path.scan(/\/[^\/]+/)
       @links = []
-      
+
       build_links!
     end
 
     def to_html
       path = File.expand_path('../../assets/breadcrumbs.eruby', __FILE__)
       template = File.read(path)
-      
+
       eruby = Erubis::Eruby.new(template)
       eruby.result(binding)
     end
@@ -28,25 +29,25 @@ module EasyBreadcrumbs
     def build_links!
       @directories.each_with_index do |directory, idx|
         full_path = @directories[0..idx].join
-        
-        if is_defined?(full_path)
-          anchor_text = if directory =~ /\d/
-                          previous_directory = @directories[idx - 1]
-                          text_for_anchor = to_anchor_text(previous_directory)
-                          singularize(text_for_anchor)
-                        else
-                          to_anchor_text(directory)
-                        end
-          @links << Link.new(full_path, anchor_text)
-        end
+
+        next unless defined_route?(full_path)
+
+        anchor_text = if directory =~ /\d/
+                        previous_directory = @directories[idx - 1]
+                        text_for_anchor = to_anchor_text(previous_directory)
+                        singularize(text_for_anchor)
+                      else
+                        to_anchor_text(directory)
+                      end
+        @links << Link.new(full_path, anchor_text)
       end
     end
 
     def to_anchor_text(directory)
-      directory.delete("/").capitalize
+      directory.delete('/').capitalize
     end
 
-    def is_defined?(path)
+    def defined_route?(path)
       @routes.any? { |route| path.match(route) }
     end
   end
