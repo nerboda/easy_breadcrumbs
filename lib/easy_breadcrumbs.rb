59634ffe2666c9012a62f19b1e92dc1a23023373
Eliathah Boda <nerboda@gmail.com>
2017-01-29 18:44:03 -0700

    Update README and remove unecessary chunk of code from lib/easy_breadcrumbs.rb.

--- lib/easy_breadcrumbs.rb
+++ lib/easy_breadcrumbs.rb
@@ -15,14 +15,6 @@ module Sinatra
       # All GET request routes
       route_matchers = self.class.routes['GET'].map { |route| route[0] }
 
-      # If there's a :home setting for the settings object, use that value
-      # Otherwise it defaults to true
-      if settings.respond_to?(:home)
-        
-      else
-
-      end
-
       # The rest is handled by Breadcrumb class
       breadcrumb = Breadcrumb.new(request_path, route_matchers)
       breadcrumb.to_html
